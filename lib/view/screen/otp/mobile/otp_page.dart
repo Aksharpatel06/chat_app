b81a9bea6464ad71d622fa832176df1c72689319
Aksharpatel06 <aksharpatel030306@gmail.com>
2024-08-23 14:55:13 +0530

    23-08-2024

--- lib/view/screen/otp/mobile/otp_page.dart
+++ lib/view/screen/otp/mobile/otp_page.dart
@@ -14,142 +14,144 @@ class OtpPage extends StatelessWidget {
     return Scaffold(
       body: SingleChildScrollView(
         child: Center(
-            child: Column(
-          children: [
-            const SizedBox(
-              height: 150,
-            ),
-            Image.asset(
-              'asset/splash/Group 1.png',
-              fit: BoxFit.cover,
-            ),
-            const SizedBox(
-              height: 100,
-            ),
-            const Text(
-              'Sign in to your Account',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 22,
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 150,
               ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 35.0,
+              Image.asset(
+                'asset/splash/Group 1.png',
+                fit: BoxFit.cover,
               ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Padding(
-                    padding: EdgeInsets.all(8.0),
-                    child: Text(
-                      'Phone Number',
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 5),
-                    child: Container(
-                      height: 50,
-                      width: double.infinity,
-                      padding: const EdgeInsets.only(left: 5),
-                      decoration: BoxDecoration(
-                          color:
-                              themeController.themeMode.value == ThemeMode.dark
-                                  ? const Color(0xff1d1d1e)
-                                  : const Color(0xffF0F0F0),
-                          borderRadius: BorderRadius.circular(50)),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          const SizedBox(
-                            width: 10,
-                          ),
-                          SizedBox(
-                            width: 40,
-                            child: TextField(
-                              controller: signController.countryController,
-                              keyboardType: TextInputType.number,
-                              decoration: const InputDecoration(
-                                border: InputBorder.none,
-                              ),
-                            ),
-                          ),
-                          const Text(
-                            "|",
-                            style: TextStyle(fontSize: 33, color: Colors.grey),
-                          ),
-                          const SizedBox(
-                            width: 10,
-                          ),
-                          Expanded(
-                              child: TextField(
-                            onChanged: (value) {
-                              signController.phone.value = value;
-                            },
-                            keyboardType: TextInputType.phone,
-                            decoration: const InputDecoration(
-                              border: InputBorder.none,
-                              hintText: "Phone",
-                            ),
-                          ))
-                        ],
+              const SizedBox(
+                height: 100,
+              ),
+              const Text(
+                'Sign in to your Account',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 22,
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 35.0,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Text(
+                        'Phone Number',
                       ),
                     ),
-                  ),
-                ],
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 5),
+                      child: Container(
+                        height: 50,
+                        width: double.infinity,
+                        padding: const EdgeInsets.only(left: 5),
+                        decoration: BoxDecoration(
+                            color: themeController.themeMode.value ==
+                                    ThemeMode.dark
+                                ? const Color(0xff1d1d1e)
+                                : const Color(0xffF0F0F0),
+                            borderRadius: BorderRadius.circular(50)),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const SizedBox(
+                              width: 10,
+                            ),
+                            SizedBox(
+                              width: 40,
+                              child: TextField(
+                                controller: signController.countryController,
+                                keyboardType: TextInputType.number,
+                                decoration: const InputDecoration(
+                                  border: InputBorder.none,
+                                ),
+                              ),
+                            ),
+                            const Text(
+                              "|",
+                              style:
+                                  TextStyle(fontSize: 33, color: Colors.grey),
+                            ),
+                            const SizedBox(
+                              width: 10,
+                            ),
+                            Expanded(
+                                child: TextField(
+                              onChanged: (value) {
+                                signController.phone.value = value;
+                              },
+                              keyboardType: TextInputType.phone,
+                              decoration: const InputDecoration(
+                                border: InputBorder.none,
+                                hintText: "Phone",
+                              ),
+                            ))
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-            Padding(
-              padding:
-                  const EdgeInsets.symmetric(horizontal: 35.0, vertical: 5),
-              child: Row(
-                children: [
-                  Checkbox(
-                    value: false,
-                    onChanged: (value) {},
-                  ),
-                  const Text('Remember me')
-                ],
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 35.0, vertical: 5),
+                child: Row(
+                  children: [
+                    Checkbox(
+                      value: false,
+                      onChanged: (value) {},
+                    ),
+                    const Text('Remember me')
+                  ],
+                ),
               ),
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 40.0),
-              child: GestureDetector(
-                onTap: () {
-                  GoogleFirebaseServices.googleFirebaseServices.mobileUser(
-                      signController.phone.value,
-                      signController.countryController.text);
-                },
-                child: Container(
-                  width: double.infinity,
-                  height: 50,
-                  alignment: Alignment.center,
-                  decoration: BoxDecoration(
-                      boxShadow: const [
-                        BoxShadow(
-                          color: Color(0x3F000000),
-                          blurRadius: 4,
-                          offset: Offset(0, 4),
-                          spreadRadius: 0,
-                        )
-                      ],
-                      color: const Color(0xff31C48D),
-                      borderRadius: BorderRadius.circular(25)),
-                  child: const Text(
-                    'Send the otp',
-                    style: TextStyle(fontSize: 17, color: Colors.white),
+              const SizedBox(
+                height: 30,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 40.0),
+                child: GestureDetector(
+                  onTap: () {
+                    GoogleFirebaseServices.googleFirebaseServices.mobileUser(
+                        signController.phone.value,
+                        signController.countryController.text);
+                  },
+                  child: Container(
+                    width: double.infinity,
+                    height: 50,
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                        boxShadow: const [
+                          BoxShadow(
+                            color: Color(0x3F000000),
+                            blurRadius: 4,
+                            offset: Offset(0, 4),
+                            spreadRadius: 0,
+                          )
+                        ],
+                        color: const Color(0xff31C48D),
+                        borderRadius: BorderRadius.circular(25)),
+                    child: const Text(
+                      'Send the otp',
+                      style: TextStyle(fontSize: 17, color: Colors.white),
+                    ),
                   ),
                 ),
               ),
-            ),
-          ],
-        )),
+            ],
+          ),
+        ),
       ),
     );
   }
