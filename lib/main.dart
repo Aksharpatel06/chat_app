2f91dc64e24e8c6be07dd0ab84a75cf2f37333ab
Aksharpatel06 <aksharpatel030306@gmail.com>
2024-09-06 19:20:35 +0530

    6-9-2024

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,14 @@
+import 'package:chat_app/routes.dart';
 import 'package:chat_app/view/controller/chat_controller.dart';
 import 'package:chat_app/view/controller/sign_controller.dart';
 import 'package:chat_app/view/controller/theme_controller.dart';
-import 'package:chat_app/view/helper/google_firebase_services.dart';
 import 'package:chat_app/view/helper/notification_services.dart';
-import 'package:chat_app/view/screen/chat/chat_page.dart';
-import 'package:chat_app/view/screen/home/home_page.dart';
-import 'package:chat_app/view/screen/intro/intro_page.dart';
-import 'package:chat_app/view/screen/otp/mobile/otp_page.dart';
-import 'package:chat_app/view/screen/otp/verify/otp_verify_page.dart';
-import 'package:chat_app/view/screen/sign/sign%20up/sign_up_page.dart';
-import 'package:chat_app/view/screen/splash/splash_page.dart';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'firebase_options.dart';
-import 'view/screen/sign/sign in/sign_in_page.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -50,44 +43,7 @@ class MyApp extends StatelessWidget {
           theme: ThemeData.light(),
           darkTheme: ThemeData.dark(),
           debugShowCheckedModeBanner: false,
-          getPages: [
-            GetPage(
-              name: '/',
-              page: () => const SplashPage(),
-            ),
-            GetPage(
-              name: '/intro',
-              page: () =>
-                  GoogleFirebaseServices.googleFirebaseServices.currentUser() !=
-                          null
-                      ? const HomePage()
-                      : const IntroPage(),
-            ),
-            GetPage(
-              name: '/signin',
-              page: () => const SignInPage(),
-            ),
-            GetPage(
-              name: '/signup',
-              page: () => const SignUpPage(),
-            ),
-            GetPage(
-              name: '/otp',
-              page: () => const OtpPage(),
-            ),
-            GetPage(
-              name: '/otpAdd',
-              page: () => const OtpVerifyPage(),
-            ),
-            GetPage(
-              name: '/home',
-              page: () => const HomePage(),
-            ),
-            GetPage(
-              name: '/chat',
-              page: () => const ChatPage(),
-            ),
-          ],
+          getPages: getPages,
         ),
       ),
     );
