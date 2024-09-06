fb31e0796c8134db06b8fa49e0c9fc3fa33fd9cb
Aksharpatel06 <aksharpatel030306@gmail.com>
2024-09-06 19:44:41 +0530

    6-9-2024

--- lib/view/helper/notification/firebase_messaging_services.dart
+++ lib/view/helper/notification/firebase_messaging_services.dart
@@ -11,6 +11,27 @@ class FirebaseMessagingServices {
 
   final FirebaseMessaging _firebaseMessaging = FirebaseMessaging.instance;
 
+  Future<void> requestPermission()
+  async {
+    NotificationSettings settings = await _firebaseMessaging.requestPermission(
+      alert: true,
+      announcement: false,
+      badge: true,
+      carPlay: false,
+      criticalAlert: false,
+      provisional: false,
+      sound: true,
+    );
+
+    if(settings.authorizationStatus==AuthorizationStatus.authorized)
+    {
+      log('Notification Permission Allowed !');
+    } else if(settings.authorizationStatus == AuthorizationStatus.denied)
+    {
+      log('Notification Permission Denied');
+    }
+  }
+
   // 4. return token
   Future<String?> generateDeviceToken() async {
     String? token = await _firebaseMessaging.getToken();
